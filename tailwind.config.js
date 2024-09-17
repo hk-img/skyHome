26900fa5417b9e4b10f6fda375d3082259772179
shubham agrawal <145680136+shubhamimg@users.noreply.github.com>
2024-09-17 17:51:14 +0530

    code

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'theme1': '#e3602d',
+        'theme2': '#1a3d6b'
+      }
+    },
+  },
+  plugins: [],
+}
